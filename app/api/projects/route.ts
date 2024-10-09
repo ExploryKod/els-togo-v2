fc8570bf40784e9ca13c1644d793f527e2ae22ea
Amaury Franssen <amauryfranssen@gmail.com>
2024-10-09 03:03:55 +0200

    fix: path rectified for projects

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,9 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import {generateUniqueFilename} from "@/utils/validations/stringManipulation";
-import path from "path";
-import fs from "fs";
-import {createClient} from "@/utils/supabase/client";
-import {useState} from "react";
+
 import {revalidatePath} from "next/cache";
 export const dynamic = 'force-dynamic' // defaults to auto
 
