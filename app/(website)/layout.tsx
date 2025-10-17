382b156ed8beaf58262abb8dbf687d700fb4469a
Amaury <amauryfranssen@gmail.com>
2025-10-17 22:40:38 +0200

    feat(head): add no index no follow

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -36,6 +36,19 @@ export async function generateMetadata(): Promise<Metadata> {
       default: title,
     },
     description: description,
+    robots: {
+      index: false,
+      follow: false,
+      nocache: true,
+      googleBot: {
+        index: false,
+        follow: false,
+        noimageindex: true,
+        'max-video-preview': -1,
+        'max-image-preview': 'none',
+        'max-snippet': -1,
+      },
+    },
     openGraph: {
       title: title,
       description: description,
